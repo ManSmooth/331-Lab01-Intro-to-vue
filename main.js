0892ae87065469c7ddb43384809d90077a0c95d8
Ax32 <130172322+thirtytwo-as@users.noreply.github.com>
2023-06-29 20:07:06 +0700

    Checkpoint 5.5

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ createApp({
 			product_desc: ref("Boots are like shoes but different."),
 			image: ref("./assets/images/socks_green.jpg"),
 			camt_url: ref("https://www.camt.cmu.ac.th"),
-            inStock: ref(true),
-            inventory: ref(10),
-            onSale: ref(true),
+			inStock: ref(true),
+			inventory: ref(10),
+			onSale: ref(true),
+			details: ref(["50% cotton", "30% wool", "20% polyester"]),
+            variants: ref([
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]),
+            sizes: ref(['S', 'M', 'L'])
 		};
 	},
 }).mount("#app");

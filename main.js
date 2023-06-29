233bb48cf8c632d0b53fe96a303ce387c4730718
Ax32 <130172322+thirtytwo-as@users.noreply.github.com>
2023-06-29 20:24:18 +0700

    Checkpoint 6.7

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-const { createApp, ref } = Vue;
+const { createApp, ref } = Vue
 createApp({
 	setup() {
+        const cart = ref(0)
+        const image = ref("./assets/images/socks_green.jpg")
+        const inventory = ref(10)
 		return {
 			product: ref("Boots"),
 			product_desc: ref("Boots are like shoes but different."),
-			image: ref("./assets/images/socks_green.jpg"),
+			image,
 			camt_url: ref("https://www.camt.cmu.ac.th"),
 			inStock: ref(true),
-			inventory: ref(10),
+			inventory,
 			onSale: ref(true),
 			details: ref(["50% cotton", "30% wool", "20% polyester"]),
             variants: ref([
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]),
-            sizes: ref(['S', 'M', 'L'])
+            sizes: ref(['S', 'M', 'L']),
+            cart,
+            AddToCart: () => cart.value++,
+            UpdateImage: (imgUrl) => image.value = imgUrl,
+            SetInventory: (amnt) => inventory.value = amnt,
 		};
 	},
 }).mount("#app");

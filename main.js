631c4c03b5777d09fbff7bd9ca4f70ce396b42f8
Ax32 <130172322+thirtytwo-as@users.noreply.github.com>
2023-06-29 19:56:51 +0700

    Checkpoint 3.6

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { createApp, ref } = Vue;
 createApp({
-    setup(){
-        return {
-            product: ref('Boots'),
-            product_desc: ref('Boots are like shoes but different.')
-        }
-    }
-}).mount('#app')
+	setup() {
+		return {
+			product: ref("Boots"),
+			product_desc: ref("Boots are like shoes but different."),
+			image: ref("./assets/images/socks_green.jpg"),
+			camt_url: ref("https://www.camt.cmu.ac.th"),
+		};
+	},
+}).mount("#app");

4f08d2f8cfd91188639eb12de120eec8c2923c65
Ax32 <130172322+thirtytwo-as@users.noreply.github.com>
2023-06-29 20:46:35 +0700

    Checkpoint 7.6 & 8.5

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue;
 createApp({
 	setup() {
-        const cart = ref(0)
-        const image = ref("./assets/images/socks_green.jpg")
-        const inventory = ref(10)
+		const cart = ref(0);
+		const onSale = ref(true);
+		const brand = ref("SE 331");
+		const product = ref("Boots");
+		const selectedVariant = ref(0);
+		const variants = ref([
+			{
+				id: 2234,
+				color: "green",
+				image: "./assets/images/socks_green.jpg",
+				quantity: 50,
+			},
+			{
+				id: 2235,
+				color: "blue",
+				image: "./assets/images/socks_blue.jpg",
+				quantity: 0,
+			},
+		]);
 		return {
-			product: ref("Boots"),
+			product,
+			brand,
 			product_desc: ref("Boots are like shoes but different."),
-			image,
+			image: computed(() => variants.value[selectedVariant.value].image),
 			camt_url: ref("https://www.camt.cmu.ac.th"),
-			inStock: ref(true),
-			inventory,
-			onSale: ref(true),
+			inStock: computed(
+				() => variants.value[selectedVariant.value].quantity > 0
+			),
+			inventory: computed(
+				() => variants.value[selectedVariant.value].quantity
+			),
+			onSale,
 			details: ref(["50% cotton", "30% wool", "20% polyester"]),
-            variants: ref([
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]),
-            sizes: ref(['S', 'M', 'L']),
-            cart,
-            AddToCart: () => cart.value++,
-            UpdateImage: (imgUrl) => image.value = imgUrl,
-            SetInventory: (amnt) => inventory.value = amnt,
+			variants,
+			sizes: ref(["S", "M", "L"]),
+			cart,
+			AddToCart: () => cart.value++,
+			UpdateVariant: (index) => (selectedVariant.value = index),
+			SetInventory: (amnt) => {
+				variants.value[selectedVariant.value].quantity = amnt;
+			},
+			title: computed(() => {
+				return `${brand.value} ${product.value} ${
+					onSale.value ? "is on sale" : ""
+				}`;
+			}),
 		};
 	},
 }).mount("#app");

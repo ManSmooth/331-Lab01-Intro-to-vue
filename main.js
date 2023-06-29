5345429df2a5ae0c531bf856116ed9a86d511a9d
Ax32 <130172322+thirtytwo-as@users.noreply.github.com>
2023-06-29 20:01:44 +0700

    Checkpoint 4.9

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ createApp({
 			product_desc: ref("Boots are like shoes but different."),
 			image: ref("./assets/images/socks_green.jpg"),
 			camt_url: ref("https://www.camt.cmu.ac.th"),
+            inStock: ref(true),
+            inventory: ref(10),
+            onSale: ref(true),
 		};
 	},
 }).mount("#app");
